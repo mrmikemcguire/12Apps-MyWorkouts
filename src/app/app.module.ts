ccb889705d142f8de623d79bcb085f44c2f73ce3
Mike McGuire <MoonshotMike@gmail.com>
2017-01-06 12:36:05 -0700

    lecture 38 finished and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { WorkoutsPage } from '../pages/workouts/workouts';
+import { AddWorkoutPage } from '../pages/add-workout/add-workout';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    WorkoutsPage,
+    AddWorkoutPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -21,9 +21,9 @@ import { TabsPage } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    WorkoutsPage,
+    AddWorkoutPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
